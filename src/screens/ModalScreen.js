f39618cbfd0b933cb2a711e2465027a4e0a79491
furyhawk <furyx@hotmail.com>
2021-04-14 21:50:28 +0800

    textinput

--- src/screens/ModalScreen.js
+++ src/screens/ModalScreen.js
@@ -1,22 +1,88 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, Text, View, TextInput } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
-function ModalScreen() {
+function ModalScreen({ navigation }) {
+    const value = ''
     return (
         <View style={styles.container}>
-            <Text>Modal Screen</Text>
+            <View style={styles.innerContainer}>
+                <View style={styles.closeButtonContainer}>
+                    <TouchableOpacity
+                        style={styles.closeButton}
+                        onPress={() => navigation.goBack()}>
+                        <Ionicons name='ios-close' color='#101010' size={40} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.modalContainer}>
+                    <Text style={{ color: '#444', fontSize: 20 }}>
+                        What do you want to do?
+                    </Text>
+                    <TextInput
+                        style={styles.textInput}
+                        numberOfLines={1}
+                        onChangeText={value => setValue(value)}
+                        value={value}
+                        clearButtonMode='while-editing'
+                    />
+                    <TouchableOpacity
+                    onPress={ () => navigation.navigate('List')}>
+                        <Ionicons name='ios-arrow-down-circle' size={40} color='#fff' />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
+        flex: 1
+    },
+    innerContainer: {
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        justifyContent: 'flex-end',
+        flexDirection: 'row',
+        height: '30%',
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        backgroundColor: '#fff'
+    },
+    closeButtonContainer: {
+        position: 'absolute',
+        alignItems: 'flex-end',
+        right: 10
+    },
+    closeButton: {
+        backgroundColor: '#d3d3d3',
+        borderRadius: 20,
+        width: 40,
+        height: 40,
+        top: 10,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    modalContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        margin: 60,
+        top: 10,
+        left: 50
+    },
+    textInput: {
+        height: 50,
+        width: 200,
+        padding: 5,
+        borderColor: 'gray',
+        borderBottomWidth: 1
     }
 })
+
+
 
 export default ModalScreen
 
