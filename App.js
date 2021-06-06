5aa95e6e84ac2794c9b61c5d8f82aee7e166d887
furyhawk <furyx@hotmail.com>
2021-06-06 09:19:07 +0800

    Material UI

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Provider as StateProvider } from 'react-redux'
 
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { red, purple } from '@material-ui/core/colors';
+
 import store from './src/redux/store'
 import MainStackNavigator from './src/navigation/AppNavigator';
 
+const primary = red[500]; // #F44336
+const accent = purple['A200']; // #E040FB
+
+const theme = createMuiTheme({
+  palette: {
+    // primary: primary,
+    // secondary: accent
+  }
+});
+
 export default function App() {
   return (
-    <StateProvider store={store}>
-      <MainStackNavigator />
-    </StateProvider>
+    <MuiThemeProvider theme={theme}>
+      <StateProvider store={store}>
+        <MainStackNavigator />
+      </StateProvider>
+    </MuiThemeProvider>
 
 
   )
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-function ListView({listItems}) {
-    return (
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+    root: {
+    flexGrow: 1
+    },
+    paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+    }
+    });
+
+// function ListView({listItems}) {
+//     return (
+        const ListView = withStyles(styles)(({ classes, listItems }) => (
         <View
             style={{
                 backgroundColor: 'white',
@@ -13,26 +29,34 @@ function ListView({listItems}) {
                 paddingVertical: 20
             }}>
             {listItems.length !== 0 ? (
+                <div className={classes.root}>
+                    <Grid container spacing={4}>
                 <FlatList
                     data={listItems}
                     keyExtractor={item => item.championId.toString()}
                     renderItem={({ item }) => (
-                        <View style={styles.listItemContainer}>
-                            <View style={styles.listItemMetaContainer}>
+                        // <View style={styles.listItemContainer}>
+                            
+                        //     <View style={styles.listItemMetaContainer}>
+                            <Grid item xs={12} sm={6} md={3}>
+                                <Paper className={classes.paper}>
                                 <Text style={styles.itemTitle} numberOfLines={1}>
                                     {item.name}
                                 </Text>
-                            </View>
-                        </View>
+                                </Paper>
+                            </Grid>
+                        //     </View>
+                        // </View>
                     )}
                 />
+                </Grid>
+                </div>
             ) : (
                 <Text style={{ fontSize: 30 }}>You list is empty :'(</Text>
             )
             }
-
         </View>
-    )
-}
+    ));
+// }
 
 export default ListView
