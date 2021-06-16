971b0339b8ef9582f08ce0b86d5dac190b60b5ef
furyhawk <furyx@hotmail.com>
2021-06-16 17:53:30 +0800

    native

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,28 +1,20 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, StatusBar, Text, View, TouchableOpacity, FlatList } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
-
 import { getChampions } from '../redux/actions'
 import Header from '../components/Header'
-import FixedPosition from '../components/FixedPosition'
 import ChampionView from '../components/ChampionView'
 
 const numColumns = 4; //number of column per row to display
 
 function ListScreen({ navigation }) {
-    // const styles = useStyles()
     return (
-        <>
             <View style={styles.container}>
-                <FixedPosition title={'TFT Champion set 5'} />
-                {/* <Header title={'TFT Champion set 5'} /> */}
+                {/* <FixedPosition title={'TFT Champion set 5'} /> */}
+                <Header title={'TFT Champion set 5'} />
                 <ListView />
                 <View style={styles.fabContainer}>
                     <TouchableOpacity
@@ -33,12 +25,10 @@ function ListScreen({ navigation }) {
                 </View>
                 <StatusBar barStyle='light-content' />
             </View>
-        </>
     )
 }
 
 function ListView() {
-    // const styles = useStyles()
     const listItems = useSelector(state => state.itemList)
     const filteredItems = useSelector(state => state.filteredList)
 
@@ -59,7 +49,7 @@ function ListView() {
                 paddingVertical: 20
             }}>
             {filteredItems.length !== 0 ? (
-                
+
                 <FlatList
                     data={filteredItems}
                     keyExtractor={item => item.championId.toString()}
@@ -67,14 +57,12 @@ function ListView() {
                     renderItem={({ item }) => (
                         <View style={styles.listItemContainer}>
                             <View style={styles.listItemMetaContainer}>
-                                <Paper>
-                                    <ChampionView champion={item}  />
-                                </Paper>
+                                <ChampionView champion={item} />
                             </View>
                         </View>
                     )}
                 />
-                
+
             ) : (
                 <Text style={{ fontSize: 30 }}>You list is empty :'(</Text>
             )
@@ -84,33 +72,6 @@ function ListView() {
     )
 }
 
-const useStyles = makeStyles(theme => ({
-    container: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
-    listItemContainer: {
-        aspectRatio: 1,
-        flex: 1 / numColumns,
-        backgroundColor: 'transparent',
-        margin: 12
-    },
-    fabContainer: {
-        justifyContent: 'flex-end',
-        flexDirection: 'row',
-        position: 'absolute',
-        right: 10,
-        bottom: 20
-    },
-    fabButton: {
-        backgroundColor: 'blue',
-        borderRadius: 35,
-        width: 70,
-        height: 70,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-  }));
 const styles = StyleSheet.create({
     container: {
         flex: 1,
