ccf4f7eadb1457f0f293cac10b0571f1f03758d7
furyhawk <furyx@hotmail.com>
2021-06-17 09:50:36 +0800

    public assets

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,7 @@
 export const ADD_ITEM = 'ADD_ITEM'
 export const GET_CHAMPIONS = 'GET_CHAMPIONS';
+export const GET_CHAMPION = 'GET_CHAMPION';
+export const SEARCH = 'SEARCH';
 
 export const addItem = item => ({
     type: ADD_ITEM,
@@ -11,7 +13,10 @@ export const getChampions = () => ({
     payload: require('../../assets/champions.json')
 })
 
-export const SEARCH = 'SEARCH';
+export const getChampion = (champion) => ({
+  type: GET_CHAMPION,
+  payload: require('../../assets/champions/' + champion.championId + '.png')
+})
 
 export function search(value) {
   return {type: SEARCH, value};
